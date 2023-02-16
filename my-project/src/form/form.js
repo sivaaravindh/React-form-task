661b7a17bccdfb01abec4f74ae1ebf1ae76aae1b
sivaaravindh <sivaaravind262003@gmail.com>
2023-02-16 10:01:42 +0530

    index

--- my-project/src/form/form.js
+++ my-project/src/form/form.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-
+import "./Form.css";
+ //import Child from "./child";
 const Form = () => {
   const [tasks, setTasks] = useState([]);
+  const [prioritys, setPriority] = useState("");
+  const [completion, setCompletion] = useState("");
 
   const addTask = (e) => {
     e.preventDefault();
@@ -13,6 +16,31 @@ const Form = () => {
     };
     setTasks([...tasks, newTask]);
   };
+  const priorityTasks = tasks.filter(task => task.isPriority===true);
+  const completeTasks = tasks.filter(task => task.isComplete===true);
+
+  
+  // let priorityTasks = tasks.filter((value) => {
+  //   return value.isPriority === true;
+  // });
+  // let completeTasks = tasks.filter((value) => {
+  //   return value.isComplete === true;
+  // })
+  
+  const Uptade = (e) => {
+    if (e.target.name === "isPrioritys" && e.target.checked ){
+      tasks[0].isPriority=true
+    }
+    setTasks([...tasks]);
+  }
+
+  const handleInput = (ev) => {
+  if (ev.target.name === "isPrioritys") {
+      setPriority(ev.target.checked);
+    } else {
+      setCompletion(ev.target.checked);
+    }
+ };
 
   // const filterTasks = (e) => {
   //   let filteredTasks = tasks.filter((task) => {
@@ -27,39 +55,138 @@ const Form = () => {
   //   setTasks(filteredTasks);
   // };
 
+  const Delete = (i) => {
+    tasks.splice(i,1);
+    setTasks([...tasks]);
+  };
+  
   return (
     <>
-      <h3>Create a new task</h3>
+    <div class="container">
+      <div class="content">
+      <h2 className='title'>Create a new task</h2>
       <form onSubmit={addTask}>
-        <label>
-          Title:
-          <input type="text" name="title" />
-        </label>
-        <label>
-          Description:
-          <input name="description"/>
-        </label>
-        <label>
-          Priority:
-          {/* <input type="checkbox" name="isPriority" onChange={filterTasks} /> */}
-          <input type="checkbox" name="isPriority"/>
-        </label>
-        <label>
-          Complete:
-          {/* <input type="checkbox" name="isComplete" onChange={filterTasks} /> */}
-          <input type="checkbox" name="isComplete"/>
-        </label>
-        <input type="submit" value="Add Task" />
-      </form>
-      <h3>Tasks</h3>
-      {tasks.map((task,i) => (
-        <div key={i}>
-          <h4>{task.title}</h4>
-          <p>{task.description}</p>
-          <p>Priority: {task.isPriority ?  (<input type="checkbox" name="isPriority" checked/>) : ( <input type="checkbox" name="isPriority"/>)}</p>
-          <p>Complete: {task.isComplete ? (<input type="checkbox" name="isComplete" checked/>): (<input type="checkbox" name="isComplete"/>)}</p>
+      <div className='h-row'>
+        <div className='h-col-1'>
+            <label class="lable">
+              Title:
+            </label>
         </div>
+        <div className='h-col-2' >
+        <input type="text" name="title" className='input' />
+        </div>
+        <div className='h-col-1'>
+        <label class="lable">
+          Description:
+        </label>
+        </div>
+        <div className='h-col-2'>
+        <input name="description" className='input'/>
+        </div> 
+        <div className='h-col-1'>
+        <label class="lable">
+          Priority:
+        </label>
+        </div>
+        <div className='h-col-2'>
+          {/* <input type="checkbox" name="isPriority" onChange={filterTasks} /> */}
+          <input type="checkbox" name="isPriority"  />
+        </div>
+     
+        <div className='h-col-1'>
+        <label class="lable">
+          Complete:
+        </label>
+        </div>
+        <div className='h-col-2'>
+          {/* <input type="checkbox" name="isComplete" onChange={filterTasks} /> */}
+          <input type="checkbox" name="isComplete"   />
+        </div>
+      <div className='btn'>
+         <input type="submit" value="Add Task" className='but'/>
+      </div>  
+      <div className='h-col-1'>
+        <label class="lable">
+          Priority task:
+        </label>
+        </div>
+        <div className='h-col-2'>
+          {/* <input type="checkbox" name="isPriority" onChange={filterTasks} /> */}
+          <input type="checkbox" name="isPrioritys" onChange={handleInput} />
+        </div>
+     
+        <div className='h-col-1'>
+        <label class="lable">
+          Complete task:
+        </label>
+        </div>
+        <div className='h-col-2'>
+          {/* <input type="checkbox" name="isComplete" onChange={filterTasks} /> */}
+          <input type="checkbox" name="isCompletes"  onChange={handleInput} />
+        </div>
+      </div>      
+      </form>
+      </div>  
+      <h2 class="line">Tasks</h2>
+      <div class="row">
+      {Boolean(prioritys) === false && Boolean(completion) === false && (
+        <div>
+        {tasks.map((task,i) => (
+          <div key={i}  class="col-1">
+            <ul>
+            <li><h4>{task.title}</h4></li>
+            <li><p>{task.description}</p></li>
+            </ul>
+            <p>Priority: {task.isPriority ?  (<input type="checkbox" name="isPriority" defaultChecked/>) : 
+                                             ( <input type="checkbox" name="isPrioritys" onChange={Uptade}/>)}</p>
+            <p>Complete: {task.isComplete ? (<input type="checkbox" name="isComplete" defaultChecked/>):
+                                            (<input type="checkbox" name="isComplete" onChange={Uptade}/>)}</p>
+            <button onClick={() => Delete(i)}>
+                  clean
+            </button>
+          </div>
+         ))}
+         </div>
+         )}
+    {Boolean(prioritys) === true && (
+        <div>
+      {priorityTasks.map((task, i) => (
+        <div key={i} class="col-2">
+           <h3>Priority Tasks</h3>
+            <ul>
+              <li><p><span> Task-name: </span>{task.title}</p></li>
+              <li><p><span> Task-description:  </span> {task.description}</p></li>
+            </ul>
+            <p>Priority: {task.isPriority ?  (<input type="checkbox" name="isPriority" defaultChecked/>) : ( <input type="checkbox" name="isPrioritys" onChange={Uptade}/>)}</p>
+            <p>Complete: {task.isComplete ? (<input type="checkbox" name="isComplete" defaultChecked/>): (<input type="checkbox" name="isComplete" onChange={Uptade}/>)}</p>
+            <button onClick={() => Delete(i)}>
+                  clean
+            </button>
+          </div>
       ))}
+        </div>
+        )}
+  {Boolean(completion) === true && (
+     <div>
+      {completeTasks.map((task, i) => (
+        <div key={i} class="col-2">
+           <h3>Complete Tasks</h3>
+            <ul>
+              <li><p><span> Task-name: </span>{task.title}</p></li>
+              <li><p><span> Task-description:  </span> {task.description}</p></li>
+            </ul>
+            <p>Priority: {task.isPriority ?  (<input type="checkbox" name="isPriority" defaultChecked/>) : ( <input type="checkbox" name="isPriority" onChange={Uptade}/>)}</p>
+            <p>Complete: {task.isComplete ? (<input type="checkbox" name="isComplete" defaultChecked/>): (<input type="checkbox" name="isComplete" onChange={Uptade}/>)}</p>
+            <button onClick={() => Delete(i)}>
+                  clean
+            </button>
+          </div>
+      ))}
+      </div>
+      )}
+       </div>
+      </div>
+      {/* <Child /> */}
     </>
   );
 };
